eb22bd8fd4512f0061b21c4299f2d2ab477a2994
Aleksandr Sedinin <alexsednin@gmail.com>
2026-07-06 00:25:38 +0300

    feat(autoload): implement autoload for src folder, implement iblock helper

---
+++ iblock-helper-demo.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Otus\Iblock\Helper;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+/**
+ * @var CMain $APPLICATION  
+ */
+
+$APPLICATION->SetTitle('Демо Helper.php и автозагрузчика');
+
+$id = Helper::getIblockIdByCode('clients_s1');
+
+echo $id;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
