5183fa2c343f517c099799def8496f25262fc57b
Aleksandr Sedinin <alexsednin@gmail.com>
2026-07-16 19:37:33 +0300

    16-07-2026 - выполнено домашнее задание 3 - реализован вывод врачей блоками, с указанием процедур, добавлен авбстрактный класс + классы для списков докторов и процедур. Добавлены стили. Добавлены новые классы в autoload

---
+++ local/lib/Models/DoctorPropertyValuesTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Models;
+
+/**
+ * Class DoctorPropertyValuesTable
+ *
+ * @package Models
+ */
+class DoctorPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    const IBLOCK_ID = 16; // ID инфоблока Доктора
+}
---
+++ local/lib/Models/ProcsPropertyValuesTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Models;
+
+/**
+ * Class ProcsPropertyValuesTable
+ *
+ * @package Models
+ */
+class ProcsPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    const IBLOCK_ID = 17; // ID инфоблока Процедуры
+}
